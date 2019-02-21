7c48bbc57b27ccde89da0154939b626b12fd9666
mauricio-jelyfish <mauricio.masias@jellyfish.co.uk>
2019-02-21 05:44:26 -0500

    welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,29 +15,30 @@
 
 <div class="container-fluid">
  
-    <header>
-        <h1>{{$pages['page_title']}}</h1>
-        <h2>{{$pages['page_subtitle']}}</h2>
-    </header>
-
-
-    <div class="row">
+    <div class="row header">
         <div class="col-sm-12 col-md-12 col-lg-12">
+            {!! $boxes[4]['box_content'] !!}
+        </div>
+    </div>
+
+
+    <div class="row belowheader">
+        <div class="col-sm-12 col-md-12 col-lg-12 date">
             {!! $boxes[0]['box_content'] !!}
         </div>
     </div>
 
 
-     <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-4">
+    <div class="row boxes">
+        <div class="col-sm-12 col-md-6 col-lg-4 box b1">
             {!! $boxes[1]['box_content'] !!}
         </div>
 
-        <div class="col-sm-12 col-md-6 col-lg-4">
+        <div class="col-sm-12 col-md-6 col-lg-4 box b2">
             {!! $boxes[2]['box_content'] !!}
         </div>
 
-        <div class="col-sm-12 col-md-6 col-lg-4">
+        <div class="col-sm-12 col-md-6 col-lg-4 box b3">
             {!! $boxes[3]['box_content'] !!}
         </div>
     </div>
