28dece57e0201b57c233e4ea89ae6a1eb4818089
Mauricio Masias <mauriciomasias@mauricios-air.home>
2019-02-22 20:05:40 +0000

    responsive states

--- cubix_dump.sql
+++ cubix_dump.sql
@@ -287,35 +287,7 @@ INSERT INTO `projects` VALUES (1,'DJ Fabrizia','img/portfolio/djfabrizia.jpg','h
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `users`
---
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Mauricio','mauricio@masias.co.uk','$2y$10$IiSguFF780fxBQbThXz4oeUkQODrvwRJ.F0oniFqqqaYy3OivdJ32','Mhc5C0Dv12DLQC8dcMGp2suhvOflVbLB6OUVIgp4DBguE8D7XMwhkyJ6OlWb','2018-01-24 21:49:34','2018-01-24 21:49:34');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
