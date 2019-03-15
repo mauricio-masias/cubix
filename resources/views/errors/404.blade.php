322b269af58d4bb4c948f9ab605b84ffe156fa6d
Mauricio Masias <mauriciomasias@mauricios-air.home>
2019-03-15 17:13:48 +0000

    404 added

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -55,16 +55,19 @@
 
 <div id="wrap" class="container wrap_404">
     
-        <div class="row header show_404">
+    <div class="row header show_404">
+            
+        <div>
             <h1>404</h1>
             <h3>Sorry, the page you looking for is probably lost in the matrix</h3>
             <br />
             <br />
             <br />
             <h2 class="desk_only"><a href="{{ url('/') }}">Cubix London</a></h2>
-            
-            
         </div>
+            
+            
+    </div>
  
 </div>
 
