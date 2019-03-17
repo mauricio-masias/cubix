e7d5ca161cd9e0ba82303814c17d79fec085f11b
Mauricio Masias <mauriciomasias@mauricios-air.home>
2019-03-17 02:59:35 +0000

    page 2 structure

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,13 +1,13 @@
 <footer>
 <div>
-@if($pages['page_footer_year']!= null)
-&copy; {{$pages['page_footer_year']}}&nbsp;&nbsp;&nbsp;
+@if($site['page_footer_year']!= null)
+&copy; {{$site['page_footer_year']}}&nbsp;&nbsp;&nbsp;
 @else
 &copy; @php echo date('Y');@endphp&nbsp;&nbsp;&nbsp;
 @endif
-@if($pages['page_footer_phone']!= null)|&nbsp;&nbsp;&nbsp;{{$pages['page_footer_phone']}}&nbsp;&nbsp;&nbsp;
+@if($site['page_footer_phone']!= null)|&nbsp;&nbsp;&nbsp;{{$site['page_footer_phone']}}&nbsp;&nbsp;&nbsp;
 @endif
-@if($pages['page_footer_email']!= null)|&nbsp;&nbsp;&nbsp;<a href="mailto:{{$pages['page_footer_email']}}">{{$pages['page_footer_email']}}</a>
+@if($site['page_footer_email']!= null)|&nbsp;&nbsp;&nbsp;<a href="mailto:{{$site['page_footer_email']}}">{{$site['page_footer_email']}}</a>
 @endif
 </div>   	
 </footer> 
@@ -15,8 +15,8 @@
 <script src="js/scripts.min.js"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-@if($pages['page_extra_js_footer']!= null)
-{!! $pages['page_extra_js_footer'] !!}
+@if($site['page_extra_js_footer']!= null)
+{!! $site['page_extra_js_footer'] !!}
 @endif
 
 </body>
